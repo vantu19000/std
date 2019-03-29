a83c887523bd462d6da3fd5794047dea5c836b2c
tu.nguyen.van <tu.nguyen.van@socialfintech.net>
2019-03-29 16:50:03 +0700

    update category home 1

---
+++ wp-content/themes/bmi-tech/layouts/category.php
@@ -0,0 +1,56 @@
+<section class="categories">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 categoryBox">
+                <h3 class="heading text-center">DANH MỤC <br>SƠN TĨNH ĐIỆN</h3>
+                <div class="category-items">
+                    <select class="classic chan">
+                        <option value="">CỬA CỔNG</option>
+                    </select>
+                    <select class="classic le">
+                        <option value="">VỎ TỦ ĐIỆN</option>
+                    </select>
+                    <select class="classic chan">
+                        <option value="">KỆ GIÁ HÀNG</option>
+                    </select>
+                    <select class="classic le">
+                        <option value="">THANG - MÁNG CÁP</option>
+                    </select>
+                    <select class="classic chan">
+                        <option value="">SẢN PHẨM NỔI BẬT</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="col-md-8 albumCate">
+
+                <div class="row" style="margin-top: 10px">
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                    <div class="col-4 col-md-4">
+                        <img src="https://giaphu.com.vn/wp-content/uploads/2018/05/he-thong-day-chuyen-son-tinh-dien.jpg" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
