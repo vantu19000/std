167fcfe767ebe46faddd20bd987f3ff0cc44e2df
tu.nguyen.van <tu.nguyen.van@socialfintech.net>
2019-04-22 09:56:04 +0700

    Update functions.php

--- wp-content/themes/bmi-tech/functions.php
+++ wp-content/themes/bmi-tech/functions.php
@@ -57,7 +57,7 @@ function bmi_widgets_init() {
 
 	register_sidebar( array(
 		'name'          => esc_html__( 'Banner trái', 'bmi' ),
-		'id'            => 'left-sidebar-2',
+		'id'            => 'left-sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'bmi' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
